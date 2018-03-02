a9304d5e71f7f9120b776f9913903a68f8b18e4f
Teimuraz <teimuraz.gordeladze@msg-systems.com>
2018-03-02 07:07:12 +0100

    Added API

--- src/main/java/JaxRsConfig.java
+++
@@ -1,9 +0,0 @@
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-/**
- * Created by gordelat on 02.03.2018.
- */
-@ApplicationPath("/api")
-public class JaxRsConfig extends Application{
-}
